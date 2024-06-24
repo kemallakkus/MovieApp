1e2c14f06e89c15010f7f6c1db5db97f14206d7c
kemallakkus <kemalllakkus@gmail.com>
2024-06-24 23:46:04 +0300

    commit

--- app/src/main/java/com/example/movieapp/di/RepositoryModule.kt
+++ app/src/main/java/com/example/movieapp/di/RepositoryModule.kt
@@ -8,10 +8,6 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 
-//Provides amacı elle provides etmek. Ek yük olur. Compiler time da zaman harcıyo provideları yapmak için. Hiltin otomatiik yapması için binds. Binds run time da hallediyo.  Birden fazla impll ı interface işaret ederse üstlerinde anatosyon kullanman lazım
-//repository vm ye hizmet ediyo gibi. vm nin yaşam döngüsüne bağlamak daha iyi. singlton yapınca app in yaşam döngüsüne bağlı.
-//Installın genel kapsamı belirtir. Viewmodel üstünde başka kapsam kullanmıcam.
-
 @Module
 @InstallIn(ViewModelComponent::class)
 abstract class RepositoryModule {
